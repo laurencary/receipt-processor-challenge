fccc820cf9e6be2dee3d4843000c5e6adbdd5edb
laurencary <armstronglg720@gmail.com>
2023-10-04 18:21:16 -0500

    Add scoring logic functions and tests

--- src/utils/scorer.js
+++ src/utils/scorer.js
@@ -1,5 +1,70 @@
-const calculateScore = (receipt) => {
-    return 0;
+
+const calculateReceiptScore = (receipt) => {
+    let points = 0;
+
+    points += scoreRetailerName(receipt.retailer);
+    points += scoreTotal(receipt.total);
+    points += scoreItems(receipt.items);
+    points += scoreDescriptions(receipt.items);
+    points += scoreDate(receipt.purchaseDate);
+    points += scoreTime(receipt.purchaseTime);
+
+    return points
 }
 
-module.exports.calculateScore = calculateScore;
+const scoreRetailerName = (string) => (string.replace(/[^0-9a-z]/gi, '').length)
+
+const scoreTotal = (totalStr) => {
+    const total = parseFloat(totalStr);
+    let points = 0;
+
+    if (total === Math.floor(total)) {
+        points += 75;
+    } else if ((total - Math.floor(total)) % .25 === 0) {
+        points += 25;
+    }
+
+    return points;
+}
+
+const scoreItems = (itemsArr) => (5 * Math.floor(itemsArr.length / 2))
+
+const _scoreDescription = (item) => {
+    const trimmed = item.shortDescription.trim();
+
+    if (trimmed.length % 3 === 0) {
+        return Math.ceil(item.price * 0.2);
+    } else {
+        return 0;
+    }
+}
+
+const scoreDescriptions = (items) => {
+    return items.reduce((acc, item) => acc + _scoreDescription(item), 0);
+}
+
+const scoreDate = (purchaseDate) => {
+    const points = purchaseDate.slice(-2) % 2 === 1 ? 6 : 0
+    return points;
+}
+
+const scoreTime = (purchaseTime) => {
+    const afterTwo = purchaseTime > "14:00";
+    const beforeFour = purchaseTime < "16:00";
+
+    if (afterTwo && beforeFour) {
+        return 10;
+    } else {
+        return 0;
+    }
+
+}
+
+
+module.exports.scoreRetailerName = scoreRetailerName;
+module.exports.scoreTotal = scoreTotal;
+module.exports.scoreItems = scoreItems;
+module.exports.scoreDate = scoreDate;
+module.exports.scoreTime = scoreTime;
+module.exports.scoreDescriptions = scoreDescriptions;
+module.exports.calculateReceiptScore = calculateReceiptScore;
--- test/processReceipt.js
+++ test/processReceipt.js
@@ -3,9 +3,82 @@ const chai = require("chai");
 const expect = chai.expect;
 
 const scorer = require('../src/utils/scorer');
+const receipt1 = {
+    "retailer": "Target",
+    "purchaseDate": "2022-01-01",
+    "purchaseTime": "13:01",
+    "items": [
+        {
+            "shortDescription": "Mountain Dew 12PK",
+            "price": "6.49"
+        }, {
+            "shortDescription": "Emils Cheese Pizza",
+            "price": "12.25"
+        }, {
+            "shortDescription": "Knorr Creamy Chicken",
+            "price": "1.26"
+        }, {
+            "shortDescription": "Doritos Nacho Cheese",
+            "price": "3.35"
+        }, {
+            "shortDescription": "   Klarbrunn 12-PK 12 FL OZ  ",
+            "price": "12.00"
+        }
+    ],
+    "total": "35.35"
+}
+
+
+const receipt2 = {
+    "retailer": "M&M Corner Market",
+    "purchaseDate": "2022-03-20",
+    "purchaseTime": "14:33",
+    "items": [
+        {
+            "shortDescription": "Gatorade",
+            "price": "2.25"
+        }, {
+            "shortDescription": "Gatorade",
+            "price": "2.25"
+        }, {
+            "shortDescription": "Gatorade",
+            "price": "2.25"
+        }, {
+            "shortDescription": "Gatorade",
+            "price": "2.25"
+        }
+    ],
+    "total": "9.00"
+}
 
 describe("Score Receipt", function () {
-    it("should return a number", function () {
-        expect(scorer.calculateScore()).to.equal(0)
+    it("should count the number of alphanumeric characters in the retailer name", function () {
+        expect(scorer.scoreRetailerName(receipt1.retailer)).to.equal(6);
+        expect(scorer.scoreRetailerName(receipt2.retailer)).to.equal(14);
+    })
+
+    it("should calculate points for total correctly", function() {
+        expect(scorer.scoreTotal("9.00")).to.equal(75);
+        expect(scorer.scoreTotal("9.20")).to.equal(0);
+        expect(scorer.scoreTotal("23.75")).to.equal(25);
+    })
+
+    it("should calculate points for items count correctly", function() {
+        expect(scorer.scoreItems(receipt1.items)).to.equal(10);
+    })
+
+    it("should add 6 points if date is odd", function () {
+        expect(scorer.scoreDate(receipt1.purchaseDate)).to.equal(6);
+        expect(scorer.scoreDate("2022-05-08")).to.equal(0);
+    })
+
+    it("should calculate points of items descriptions correctly", function() {
+        expect(scorer.scoreDescriptions(receipt1.items)).to.equal(6)
+        expect(scorer.scoreDescriptions(receipt2.items)).to.equal(0)
+    })
+
+    it("should calculate points for the entire receipt", function() {
+        expect(scorer.calculateReceiptScore(receipt1)).to.equal(28)
+        expect(scorer.calculateReceiptScore(receipt2)).to.equal(109)
     })
 })
